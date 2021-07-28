27aa4b2d9d846dea5c27e2180eabcaea91430ac4
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-28 17:31:40 -0500

    djjd

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,47 @@ import React from 'react';
 import './App.css';
 //cmpnts
 import Header from './components/Header'
+import { Provider, connect} from 'react-redux';
+import { createStore} from 'redux'
 
-function App() {
+
+const reducer = (state={kk:'jfjfj'}, action) => {
+  switch (action.type) {
+    case 'kk':
+      return {
+        kk: 'kkkkk'
+      }
+    default:
+      return state;
+  }
+}
+const store = createStore(reducer);
+function Comp({state, change}) {
   return (
     <div id='quote-box'>
       <Header />
+      <div>{state.kk}</div>
     </div>
   );
+}
+const mapStateToProps = state => {
+  return {
+    state: state,
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    change: () => dispatch({type: 'kk'}),
+  }
+}
+const Container = connect(mapStateToProps, mapDispatchToProps)(Comp)
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Container />
+    </Provider>
+    )
 }
 
 export default App;
